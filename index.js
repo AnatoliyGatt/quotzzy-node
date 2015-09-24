cefafd3e28f0ffd77a35c1b30d0e94250d3c08af
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-09-24 09:52:24 +0200

    Replace " with ' in definitions of all strings in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  * @license MIT
  */
 
-"use strict";
+'use strict';
 
 /**
  * @public
@@ -15,4 +15,4 @@
  * @returns {Function} - Function to initialize unchangeable instance of Quotzzy.
  */
 
-module.exports = require("./lib/quotzzy");
+module.exports = require('./lib/quotzzy');
